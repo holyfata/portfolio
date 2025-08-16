3f7fe1a0f84a62a07c6ed902ebcbf3511d27c224
holyfata <holyfata@qq.com>
2025-08-17 05:14:12 +0800

    feat: improve the image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,14 +83,27 @@ export default function Home() {
           </p>
         </section>
         <section className="flex flex-col shrink-0 w-[240]">
-          <Image
-            aria-hidden
-            src="https://avatars.githubusercontent.com/u/206218794?v=4"
-            alt="Github Avatar"
-            width={240}
-            height={240}
-            className="rounded-full"
-          />
+          <div className="w-[240] h-[240] relative">
+            <Image
+              aria-hidden
+              src="https://avatars.githubusercontent.com/u/206218794?v=4"
+              alt="Github Avatar"
+              width={240}
+              height={240}
+              className="rounded-full"
+            />
+            <div
+              className="absolute w-[45] h-[45] rounded-full overflow-hidden"
+              style={{ bottom: 0, right: 0 }}
+            >
+              <Image
+                src="/images/chrome.gif"
+                width={45}
+                height={45}
+                alt="Chrome Icon"
+              />
+            </div>
+          </div>
           <ul
             className="flex flex-row gap-2 mt-4"
             style={{ fontFamily: "Telma-Light" }}

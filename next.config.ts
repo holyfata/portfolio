bf08e69f368d22b59eb92057d1791f4c6d26089c
holyfata <holyfata@qq.com>
2025-08-16 22:12:57 +0800

    feat: 添加最近关注的项目

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,12 @@ const nextConfig: NextConfig = {
       new URL(
         "https://img11.360buyimg.com/img/jfs/t1/275721/2/1900/17609/67d15ec5Fbfdce671/4f23c5ca45c211c7.png",
       ),
+      new URL(
+        "https://img20.360buyimg.com/ling/jfs/t1/20876/36/12835/3043/5c9c2929Ed18cfb11/15b1c03ec830ab8e.png"
+      ),
+      new URL(
+        "https://assets.rspack.rs/rslint/rslint-logo.svg"
+      )
     ],
   },
 };

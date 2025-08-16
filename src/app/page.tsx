d8f1ebabb364fe6ff56420b735c6a7aa38e7e5c1
holyfata <holyfata@qq.com>
2025-08-17 04:46:57 +0800

    feat: make the intro colorful

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,37 @@ export default function Home() {
           className="pr-4 leading-8 text-gray-500 flex-1 break-words"
           style={{ fontFamily: "Chubbo-Regular" }}
         >
-          <p style={{ fontFamily: "Comico-Regular" }}>Hey! I&apos;m HolyFata</p>{" "}
+          <p style={{ fontFamily: "Comico-Regular" }}>
+            Hey! I&apos;m HolyFata 😘
+          </p>{" "}
           <br />
           <p className="leading-10">
-            Graduated from NJUPT with a major in Biomedical Engineering, and
-            entered &nbsp;
+            Graduated from &nbsp;
+            <a
+              className="cursor-pointer hover:underline text-[#007aff]"
+              href="https://www.njupt.edu.cn/"
+              target="_blank"
+            >
+              <span
+                className="inline-block bg-cover size-[20] mr-1"
+                style={{
+                  backgroundImage: `url(https://www.njupt.edu.cn/_upload/tpl/0d/78/3448/template3448/image/favicon.ico)`,
+                }}
+              ></span>
+              NJUPT
+            </a>{" "}
+            &nbsp; with a major in Biomedical Engineering, and entered &nbsp;
             <a
               className="cursor-pointer hover:underline text-[#CC0000]"
               href="https://www.jd.com"
+              target="_blank"
             >
+              <span
+                className="inline-block bg-cover size-[20] mr-1"
+                style={{
+                  backgroundImage: `url(https://img11.360buyimg.com/img/jfs/t1/275721/2/1900/17609/67d15ec5Fbfdce671/4f23c5ca45c211c7.png)`,
+                }}
+              ></span>
               JD.COM
             </a>
             &nbsp; through the 2024 campus recruitment to engage in front-end
@@ -28,8 +50,36 @@ export default function Home() {
           <br />
           <p className="leading-10">
             I love coding and enjoy the feeling of creation. What&apos;s more, I
-            embrace open source. Apart from work, I spend my office hours
-            slacking off and browsing GitHub every day.
+            embrace &nbsp;
+            <a
+              className="cursor-pointer hover:underline text-[#3Ea638]"
+              href="https://opensource.org/"
+              target="_blank"
+            >
+              <span
+                className="inline-block bg-cover size-[20] mr-1"
+                style={{
+                  backgroundImage: `url(https://th.bing.com/th/id/ODF.89jlCB_iVlzKm_08deitXA?w=32&h=32&qlt=93&pcl=fffffa&o=6&pid=1.2)`,
+                }}
+              ></span>
+              Open Source
+            </a>
+            . Apart from work, I spend my office hours slacking off and browsing
+            &nbsp;
+            <a
+              className="cursor-pointer hover:underline text-[#1f2328]"
+              href="https://github.com/"
+              target="_blank"
+            >
+              <span
+                className="inline-block bg-cover size-[20] mr-1"
+                style={{
+                  backgroundImage: `url(https://img.icons8.com/?size=100&id=HW77LO6vM3AB&format=png&color=000000)`,
+                }}
+              ></span>
+              GitHub
+            </a>{" "}
+            &nbsp; every day.
           </p>
         </section>
         <section className="flex flex-col shrink-0 w-[240]">
--- src/components/RecentWork.tsx
+++ src/components/RecentWork.tsx
@@ -13,9 +13,15 @@ const RencentWork = () => {
             key={index}
             className="flex items-center border border-gray-200 rounded-lg py-4 px-6 cursor-pointer hover:border-gray-400 transition-colors duration-200"
           >
-            <Link href={jumpLink} target="_blank" className="flex items-center">
+            <Link
+              href={jumpLink}
+              target="_blank"
+              className="flex items-center w-full"
+            >
               <Image src={logoUrl} width={120} height={60} alt={name} />
-              <p className="text-gray-400 text-base ml-4">{description}</p>
+              <p className="text-gray-400 flex-1 text-base ml-4 flex flex-row-reverse">
+                {description}
+              </p>
             </Link>
           </li>
         );
